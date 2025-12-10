4c0aba08b2f79644f6b996a58cc52b56a7ddac7a
Cian Hughes <chughes000@gmail.com>
2025-12-10 17:40:27 +0000

    Improved builder script

--- scripts/build_subheader.py
+++ scripts/build_subheader.py
@@ -1,9 +1,15 @@
 import base64
 from jinja2 import Template
+from pathlib import Path
+import mimetypes
 import requests
 
 
-SUBHEADER_FILENAME: str = "subheader.svg"
+SCRIPT_DIR = Path(__file__).parent
+REPO_ROOT = SCRIPT_DIR.parent
+TEMPLATE_PATH = REPO_ROOT / "templates/subheader.svg.j2"
+OUTPUT_PATH = REPO_ROOT / "subheader.svg"
+
 IMAGE_URL_MAP: dict[str, str] = {
     "python": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
     "rust": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-original.svg",
@@ -17,40 +23,44 @@ IMAGE_URL_MAP: dict[str, str] = {
 }
 
 
-def get_base64(url: str) -> str:
+def get_base64_from_url(url: str) -> str:
     print(f"Downloading {url}...")
-    response = requests.get(url)
-    if url.endswith(".gif"):
-        media_type = "image/gif"
-    elif url.endswith(".svg"):
-        media_type = "image/svg+xml"
-    else:
-        raise ValueError("URL must be .gif or .svg")
-    b64_data = base64.b64encode(response.content).decode('utf-8')
-    return f"data:{media_type};base64,{b64_data}"
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+
+        content_type = response.headers.get('Content-Type')
+        if not content_type:
+             content_type, _ = mimetypes.guess_type(url)
+
+        b64_data = base64.b64encode(response.content).decode('utf-8')
+        return f"data:{content_type};base64,{b64_data}"
+
+    except requests.RequestException as e:
+        print(f"Error fetching {url}: {e}")
+        return ""
 
 
 def fetch_images_from_url_values(image_map: dict[str, str]) -> dict[str, str]:
-    return {k: get_base64(v) for k, v in image_map.items()}
+    return {k: get_base64_from_url(v) for k, v in image_map.items()}
 
 
-def render_template(template_filename: str, token_map: dict[str, str]) -> str:
-    with open(template_filename, "rt") as f:
+def render_template(template_filepath: Path, token_map: dict[str, str]) -> str:
+    with template_filepath.open("rt") as f:
         template = f.read()
     return Template(template).render(**token_map)
 
 
-def build_svg(filename: str, token_map: dict[str, str]):
-    template_filename = f"templates/{filename}.j2"
-    svg_template = render_template(template_filename, token_map)
-    with open(filename, "wt+") as f:
+def build_svg(filepath: Path, template_filepath: Path, token_map: dict[str, str]):
+    svg_template = render_template(template_filepath, token_map)
+    with filepath.open("wt+") as f:
         f.write(svg_template)
 
 
 def main():
     token_map = fetch_images_from_url_values(IMAGE_URL_MAP)
-    build_svg(SUBHEADER_FILENAME, token_map)
-    print(f"Finished building {SUBHEADER_FILENAME}")
+    build_svg(OUTPUT_PATH, TEMPLATE_PATH, token_map)
+    print(f"Finished building {OUTPUT_PATH}")
 
 
 if __name__ == "__main__":
